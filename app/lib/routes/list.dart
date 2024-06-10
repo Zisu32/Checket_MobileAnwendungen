b304f6f6882b0fc63ee38cd4e04ebcf863cd2145
Zisu32 <pas_loves_nikes@hotmail.de>
2024-06-10 15:46:31 +0200

    ListPage Report

--- app/lib/routes/list.dart
+++ app/lib/routes/list.dart
@@ -1,21 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:app/routes/footer_menu.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:io';
 
-class ListPage extends StatelessWidget{
+class ListPage extends StatefulWidget {
   const ListPage({super.key});
+
+  @override
+  _ListPageState createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+  String? pdfPath;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        bottomNavigationBar: CommenFooterMenu(context).getFooterMenu(2),
-        body: Center(
-            child: ElevatedButton(
-              onPressed: () {
-
-              },
-              child: const Text('ListPage'),
-            )
-        )
+      backgroundColor: Colors.grey[850],
+      bottomNavigationBar: CommenFooterMenu(context).getFooterMenu(2), // Assuming you have a default constructor or handling in the class
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton.icon(
+                onPressed: () async {
+                  var status = await Permission.storage.request();
+                  if (status.isGranted) {
+                    final picker = ImagePicker();
+                    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+                    if (pickedFile != null) {
+                      setState(() {
+                        pdfPath = pickedFile.path;
+                      });
+                    }
+                  }
+                },
+                icon: const Icon(Icons.download),
+                label: const Text('Report'),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                  backgroundColor: Colors.deepPurple,
+                  foregroundColor: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
