1c71ae38ac80b8a50a9dff6614efec73b9c07f29
Zisu32 <pas_loves_nikes@hotmail.de>
2024-06-24 22:41:35 +0200

    added request for create and update jacket

--- app/backend/server.js
+++ app/backend/server.js
@@ -60,6 +60,67 @@ app.post('/login', async (req, res) => {
 
 });
 
+//create Jacket
+app.post('/createJacket', async (req, res) => {
+    const { jacketNumber, qrString, imagePath} = req.body;
+
+    // Check if jacket already exists
+    const existingjacketNumber = await prisma.jacket.findUnique({
+      where: {
+        jacketNumber: jacketNumber,
+      },
+    });
+
+    if (existingjacketNumber) {
+      return res.status(409).json({ message: "JacketNumber schon vergeben" });
+    }
+
+    // create jacket
+    try {
+      const newJacket = await prisma.jacket.create({
+        data: {
+          jacketNumber,
+          qrString,
+          imagePath,
+        },
+      });
+      res.json(newJacket);
+    } catch (error) {
+      console.error(error);
+      res.status(400).json({ error: error.message });
+    }
+  });
+
+//update Jacket Status
+app.put('/updateJacketStatus', async (req, res) => {
+    const { jacketNumber, status } = req.body;
+
+    // Check if jacket Number exists
+    const number = await prisma.jacket.findUnique({
+      where: {
+        jacketNumber : jacketNumber,
+      },
+    });
+
+    if (!number) {
+      return res.status(409).json({ message: "Status konnte nicht aktualisiert werden!" });
+    }
+
+    //if (status === Status.verfuegbar)
+    console.log(typeof status)
+    //update Jacket Status
+    try {
+      const updateJacketStatus = await prisma.jacket.update({
+        where: {jacketNumber : jacketNumber},
+        data: {status: status},
+      });
+      res.json(updateJacketStatus);
+    } catch (error) {
+      console.error(error);
+      res.status(400).json({ error: error.message });
+    }
+  });
+
 // Server running port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
