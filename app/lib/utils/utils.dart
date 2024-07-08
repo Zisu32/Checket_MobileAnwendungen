2f026434c95210abe280c293725326ef7f7edb9c
Zisu32 <pas_loves_nikes@hotmail.de>
2024-07-08 19:47:44 +0200

    added ip adress in utils.dart

--- app/lib/utils/utils.dart
+++ app/lib/utils/utils.dart
@@ -17,7 +17,7 @@ String getIpAddress()
 {
   //final String ip = "192.168.178.56";
   //final String ip = "192.168.0.211";
-  final String ip = "192.168.56.1";
-  //final String ip = "10.0.2.2";
+  //final String ip = "192.168.56.1";
+  final String ip = "10.0.2.2";
   return ip;
 }
